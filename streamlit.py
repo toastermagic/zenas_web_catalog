01e4a4975803e372e1eaee97c4213280a0595c13
easy-going-paul <toastermagic@gmail.com>
2022-11-13 15:10:06 +0000

    wonderful

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit
 import snowflake.connector
 import pandas
-streamlit.title('EGP\'s Astonishing Webstore')
+streamlit.title('EGP\'s Wonderful Webstore')
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
